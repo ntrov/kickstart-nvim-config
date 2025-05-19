f6916aa1fbf51e619548083d8fe819605c9daa6e
Wajahat Raza <wajahat.raza@citrusbits.com>
2025-05-19 09:40:41 +0500

    add harpoon config ;

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      {
+        '<leader>a',
+        function()
+          require('harpoon'):list():add()
+        end,
+      },
+      {
+        '<C-e>',
+        function()
+          require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+        end,
+      },
+      {
+        '<leader>1',
+        function()
+          require('harpoon'):list():select(1)
+        end,
+      },
+      {
+        '<leader>2',
+        function()
+          require('harpoon'):list():select(2)
+        end,
+      },
+      {
+        '<leader>3',
+        function()
+          require('harpoon'):list():select(3)
+        end,
+      },
+      {
+        '<leader>4',
+        function()
+          require('harpoon'):list():select(4)
+        end,
+      },
+      {
+        '<C-S-P>',
+        function()
+          require('harpoon'):list():prev()
+        end,
+      },
+      {
+        '<C-S-N>',
+        function()
+          require('harpoon'):list():next()
+        end,
+      },
+    },
+  },
+}
